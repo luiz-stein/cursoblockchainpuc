c1a9e4fa698b3edbded70f6e58bef1822603beca
luiz-stein <56131141+luiz-stein@users.noreply.github.com>
2019-10-10 22:17:38 -0300

    jeffe prestes

--- aluguel.sol
+++ aluguel.sol
@@ -1,82 +1,67 @@
+/*
+(c) Desenvolvido por Jeff Prestes
+This work is licensed under a Creative Commons Attribution 4.0 International License.
+*/
 pragma solidity 0.5.12;
 
-/*
-
-- não ha decimais em solidity
-- uint256 = uint (inteiros) em bytes
-- address = endereco ethereum
-- constant = constante
-- memory = informa o compilador para nao armazenar no blockchain
-- view =  use view if your function does not modify storage
-
-*/
-
-contract Aluguel
+contract Aluguel 
 {
     string public locatario;
     string public locador;
     uint256 private valor;
-    
-    uint8 constant numero_maximo_legal_de_alugueis_para_multa = 3;
-    
-    //
-    
-    constructor( string memory nome_do_locador, string memory nome_do_locatario, uint256 valor_do_aluguel ) public
+    uint256 constant public numeroMaximoLegalDeAlgueisParaMulta = 3;
+    bool[] public statusPagamento;
+    address payable public contaLocatario;
+
+    constructor(string memory nomeLocador, string memory nomeLocatario, address payable paramContaLocatario, uint256 valorDoAluguel) public 
     {
-        locador = nome_do_locador;
-        locatario = nome_do_locatario;
-        valor = valor_do_aluguel;
+        locador = nomeLocador;
+        locatario = nomeLocatario;
+        valor = valorDoAluguel;
+        contaLocatario = paramContaLocatario;
     }
-    
-    //
-    
-    function ValorAtualDoAluguel() public view returns(uint256)
+ 
+    function valorAtualDoAluguel() public view returns (uint256) 
     {
         return valor;
     }
-    
-    //
-    
-    function SimulaMulta( uint256 meses_restantes, uint256 valor_meses_do_contrato) public view returns(uint256 valor_da_multa)
+ 
+    function simulaMulta( uint256 mesesRestantes, uint256 totalMesesContrato) public view returns(uint256 valorMulta) 
     {
-        valor_da_multa = valor * numero_maximo_legal_de_alugueis_para_multa;
-        valor_da_multa = valor_da_multa / valor_meses_do_contrato;
-        valor_da_multa = valor_da_multa * meses_restantes;
+        valorMulta = valor*numeroMaximoLegalDeAlgueisParaMulta;
+        valorMulta = valorMulta/totalMesesContrato;
+        valorMulta = valorMulta*mesesRestantes;
+        return valorMulta;
+    } 
         
-        return valor_da_multa;
-    }
-    
-    //
-    
-    function ReajusteAluguel( uint256 percentual_reajuste) public
+    function reajustaAluguel(uint256 percentualReajuste) public 
     {
-        if (percentual_reajuste > 20)
+        if (percentualReajuste > 20) 
         {
-            percentual_reajuste = 20;
+            percentualReajuste = 20;
         }
-        
-        uint256 valor_do_acrescimo=0;
-        valor_do_acrescimo = (valor*percentual_reajuste)/100;
-        valor = valor + valor_do_acrescimo;
+        uint256 valorDoAcrescimo = 0;
+        valorDoAcrescimo = ((valor*percentualReajuste)/100);
+        valor = valor + valorDoAcrescimo;
     }
     
-    //
-    
-    function AditamentoValorAluguel(uint256 valor_certo) public
+    function aditamentoValorAluguel(uint256 valorCerto) public
     {
-        valor = valor_certo;
+        valor = valorCerto;
     }
-    
-    //
-    
-    function AplicaMulta(uint256 meses_restantes, uint256 percentual) public
-    {
-        require( meses_restantes < 30, "Periodo de contrato invalido");
-        
-        for (uint256 i=1; i<meses_restantes; i++)
-        {
-            valor = valor + ((valor*percentual)/100);
-        }
-    }
-}
 
+    function aplicaMulta(uint256 mesesRestantes, uint256 percentual) public
+    {
+        require(mesesRestantes<30, "Periodo de contrato inválido");
+        for (uint i=1; i<mesesRestantes; i++) {
+            valor = valor+((valor*percentual)/100);
+        }
+    }
+    
+    function receberPagamento() public payable {
+        require(msg.value>=valor, "Valor insuficiente");
+        contaLocatario.transfer(msg.value);
+        statusPagamento.push(true);
+    }
+    
+}
